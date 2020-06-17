d1e5169d343f217de013b624fdd0960e3b1b7ef4
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 16:12:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fruits = ["strawberry", "watermelon", "lychee", "mango"]
+ 
     fruits.push ("grapes")
       puts fruits.inspect
       # > ["strawberry", "watermelon", "lychee", "mango", "grapes"]
