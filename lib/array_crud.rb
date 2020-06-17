f5baa80a851934f24cfe21ea9836994ce7461961
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-17 16:18:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,10 +8,9 @@ end
 
 def add_element_to_end_of_array(array, element)
     array = ["wow", "I", "am", "really", "learning"]
-    element = ("arrays!")
-    array << "arrays!"
-      puts array.inspect
-    
+      element = ("arrays!")
+        array << "arrays!"
+          puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
